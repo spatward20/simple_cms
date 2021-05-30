719a4cbabc9a63304156ad84afb35e99d0e7d2f2
Shivangi Patwardhan <shivangi.patwardhan@instacart.com>
2021-05-30 19:15:26 -0400

    controllers, views and dynamic content

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,10 +3,34 @@ class DemoController < ApplicationController
   layout false
 
   def index
+
     # render('hello')
   end
 
   def hello
+    # instance variables are used to pass data to the view
+    @array = [1, 2, 3, 4, 5, 6]
+
+    @id = params[:id]
+    @page = params[:page]
+
+    # redirect_to(:controller => 'demo', :action => 'index')
     # render('demo/index')
+  end
+
+  def about
+    render('demo/about_us')
+  end
+
+  def contact
+    case params[:country]
+    when 'us' || 'ca'
+      @phone = "(800) 555 6789"
+    when 'uk'
+      @phone = "(202) 7946 1234"
+    else
+      @phone = "+1 987 829 - 8939"
+    end
+    render('demo/contact_us')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'demo/index'
   get 'demo/hello'
 
+  match "demo/about_us", :to => "demo#about", :via => :get
+  match "demo/contact_us", :to => "demo#contact", :via => :get
+
   # default route
   # get ':controller(/:action(/:id))'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
