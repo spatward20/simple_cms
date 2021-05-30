006ad2365cefa66f6cb0725c41146705b88ca9e9
Shivangi Patwardhan <shivangi.patwardhan@instacart.com>
2021-05-30 18:16:52 -0400

    Getting stated with Ruby On Rails

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,12 @@
+class DemoController < ApplicationController
+
+  layout false
+
+  def index
+    # render('hello')
+  end
+
+  def hello
+    # render('demo/index')
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root 'demo#index'
+
+  get 'demo/index'
+  get 'demo/hello'
+
+  # default route
+  # get ':controller(/:action(/:id))'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
